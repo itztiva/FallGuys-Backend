05bc58ebac8d92b20b3d9abe6f62079930697e1d
itztiva <98614842+itztiva@users.noreply.github.com>
2024-04-08 21:59:40 -0400

    better

--- main/routes/main.js
+++ main/routes/main.js
@@ -1,6 +1,8 @@
 const Express = require("express");
 const express = Express();
+ 
 
+// test thing that will be removed soon
 express.get("/testt", async (req, res) => {
     res.json({
         metadata: {
